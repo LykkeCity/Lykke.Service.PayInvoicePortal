eb112b11447242816ea648e9dc0d303cf51d7d1f
olegtrofimenko <oleg.trofimenko@lykke.com>
2017-12-22 11:08:15 +0500

    todos

--- src/Lykke.Pay.Invoice/wwwroot/js/profile.js
+++ src/Lykke.Pay.Invoice/wwwroot/js/profile.js
@@ -10,7 +10,7 @@ $('.btn_create').on('click', function (e) {
     $('.create.draft').addClass('create--open');
 });
 
-$('body').on('click', function () {
+$('body').on('click', function (e) {
     $('body').removeClass('body--menu_opened');
     $('.create.draft').removeClass('create--open');
     $('.create.unpaid').removeClass('create--open');
@@ -18,8 +18,11 @@ $('body').on('click', function () {
 $('.create.draft').on('click', function (e) {
     e.stopPropagation();
 });
+$('.create.unpaid').on('click', function (e) {
+    e.stopPropagation();
+});
 
-$(document).ready(function () {
+$(document).ready(function (e) {
 
     updateGrid();
     updateBalance();
@@ -35,6 +38,14 @@ $(document).ready(function () {
         if ($('#Currency').val() == "")
             $('#Currency').val("USD");
         updateGrid();
+    });
+    $('.icon.icon--copy').on('click', function (e) {
+        e.stopPropagation();
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($('#UnpaidUrl').text()).select();
+        document.execCommand("copy");
+        $temp.remove();
     });
     $('#draftbtn').on('click', function (e) {
         $('#Status').val("Draft");
