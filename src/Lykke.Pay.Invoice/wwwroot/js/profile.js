63fc8feeeb680aad21e6d386166d2efe479d7f31
olegtrofimenko <oleg.trofimenko@lykke.com>
2017-12-29 10:27:14 +0500

    LPDEV-44

--- src/Lykke.Pay.Invoice/wwwroot/js/profile.js
+++ src/Lykke.Pay.Invoice/wwwroot/js/profile.js
@@ -225,6 +225,7 @@ function showItem(invoice) {
             url.innerText = window.location.origin + "/invoice/" + value;
         }
     }
+    $('#UnpaidInvoiceNumberHeader').text(" Invoice #" + invoice.InvoiceNumber);
 }
 function setTooltip(message) {
     $('.create__item-copy')
