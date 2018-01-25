0cac89c4c43cf699e1e55cb528b4473263fb259b
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2018-01-25 15:44:10 +0300

    Test project added

---
+++ tests/Lykke.Service.PayInvoicePortal.Tests/AuthControllerTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lykke.Service.PayInvoicePortal.Tests
+{
+    [TestClass]
+    public class AuthControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
