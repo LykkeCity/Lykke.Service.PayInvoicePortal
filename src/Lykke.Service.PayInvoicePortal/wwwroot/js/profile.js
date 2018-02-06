7874f523cdc4b8dd71d48eebeec680761995795e
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2018-02-06 19:19:43 +0300

    LPDEV-163 Currency display fixed when invoice created

--- src/Lykke.Service.PayInvoicePortal/wwwroot/js/profile.js
+++ src/Lykke.Service.PayInvoicePortal/wwwroot/js/profile.js
@@ -224,7 +224,7 @@ function showItem(invoice) {
             value = "";
         var item = document.getElementById("Unpaid" + keyNames[j]);
         if (keyNames[j] === "Amount") {
-            value = currentItem[keyNames[j]] + " " + currentItem["SettlementAssetId"];
+            value = currentItem[keyNames[j]];
         }
         if (keyNames[j] === "CreatedDate" || keyNames[j] === "DueDate")
             value = value.substr(0, 10);
