5fc0577b0f1541bf1588379d37309e84a16b69b6
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2018-04-09 01:40:13 +0300

    LPDEV-279 Fixed email sent tooltip

--- src/Lykke.Service.PayInvoicePortal/wwwroot/src/app/directives/tooltip.js
+++ src/Lykke.Service.PayInvoicePortal/wwwroot/src/app/directives/tooltip.js
@@ -3,43 +3,39 @@
 
     angular
         .module('app')
-        .directive('copy', ['$timeout', copy]);
+        .directive('tooltip', ['$timeout', tooltip]);
 
-    function copy($timeout) {
+    function tooltip($timeout) {
         var directive = {
             restrict: 'A',
             scope: {
-                value: '=copy',
+                trigger: '&trigger',
                 title: '@title'
             },
             link: function (scope, element, attributes) {
-                var clipboard = new Clipboard(element[0], {
-                    text: function () {
-                        return scope.value;
-                    }
-                });
-
                 var timeout;
                 var tooltip = $(element).tooltip({ 'title': scope.title, 'placement': 'top', 'trigger': 'manual' });
-                
-                clipboard.on('success', function (e) {
-                    e.clearSelection();
-                    tooltip.tooltip('show');
 
-                    if (angular.isDefined(timeout)) {
-                        $timeout.cancel(timeout);
+                scope.trigger({
+                    handler: function() {
+                        tooltip.tooltip('show');
+                        if (angular.isDefined(timeout)) {
+                            $timeout.cancel(timeout);
+                        }
+                        timeout = $timeout(function () {
+                            tooltip.tooltip('hide');
+                        }, 2000);
                     }
-                    timeout = $timeout(function() {
-                        tooltip.tooltip('hide');
-                    }, 2000);
                 });
 
                 scope.$on('$destroy', function () {
                     if (angular.isDefined(timeout)) {
                         $timeout.cancel(timeout);
                     }
-                    clipboard.destroy();
                     tooltip.tooltip('destroy');
+                    scope.trigger({
+                        handler: undefined
+                    });
                 });
             }
         };
