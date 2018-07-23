ccfd96a80d7e8536b433100aca77548b6669b04d
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-07-23 17:27:51 +0300

    LPDEV-704 upgrade logging

--- src/Lykke.Service.PayInvoicePortal.Core/Extensions/LogExtensions.cs
+++ src/Lykke.Service.PayInvoicePortal.Core/Extensions/LogExtensions.cs
@@ -7,36 +7,12 @@ namespace Lykke.Service.PayInvoicePortal.Core.Extensions
 {
     public static class LogExtensions
     {
-        public static void Info(
-            this ILog log,
-            string message,
-            object details)
-        {
-            log.Info(message, context: $"details: {details.ToJson()}");
-        }
-
-        public static void Warning(
-            this ILog log,
-            string message,
-            object details)
-        {
-            log.Warning(message, null, $"details: {details.ToJson()}");
-        }
-
         public static void Error(
             this ILog log,
             Exception exception,
             object details)
         {
-            log.Error(exception, null, $"details: {details.ToJson()}");
-        }
-
-        public static void Error(
-            this ILog log,
-            string message,
-            object details)
-        {
-            log.Error(null, message, $"details: {details.ToJson()}");
+            log.Error(exception, null, details);
         }
 
         public static void Error(
