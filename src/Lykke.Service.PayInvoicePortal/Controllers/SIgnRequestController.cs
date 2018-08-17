b1f4229457a12fb076955a4a5b104bcd46687b7c
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-08-17 12:29:41 +0300

    LPDEV0773 add begin of sign request view

---
+++ src/Lykke.Service.PayInvoicePortal/Controllers/SIgnRequestController.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using Common.Log;
+using Lykke.Common.Log;
+using Lykke.Service.PayInvoicePortal.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lykke.Service.PayInvoicePortal.Controllers
+{
+    [Authorize]
+    [Route("signrequest")]
+    public class SignRequestController : Controller
+    {
+        private readonly ILog _log;
+
+        public SignRequestController(
+            ILogFactory logFactory)
+        {
+            _log = logFactory.CreateLog(this);
+        }
+
+        [HttpGet]
+        public IActionResult SignRequest()
+        {
+            _log.Info($"SignRequest opened by {HttpContext?.Connection?.RemoteIpAddress?.ToString().SanitizeIp()}");
+
+            ViewBag.MerchantId = User.GetMerchantId();
+
+            return View();
+        }
+    }
+}
