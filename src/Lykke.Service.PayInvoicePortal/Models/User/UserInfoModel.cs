e54a888bdf734ab9723431bb0222a9742c4a9112
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-10-24 22:39:06 +0300

    LPDEV-928 fix text if there are no payments for the selected period on the payments page

---
+++ src/Lykke.Service.PayInvoicePortal/Models/User/UserInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lykke.Service.PayInvoicePortal.Models.User
+{
+    public class UserInfoModel
+    {
+        public string EmployeeId { get; set; }
+        public string EmployeeEmail { get; set; }
+        public string FullName { get; set; }
+        public string MerchantId { get; set; }
+    }
+}
